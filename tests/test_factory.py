bed41fff1dc3dfadc328a2f71ad65789cc54bb43
Gabriel Fioravante Jorge <GFJorge@santafilomena.local>
2020-09-02 17:31:55 -0300

    refac

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,5 +1,5 @@
 def test_pytest_environment(app):
-    assert app.debug  # this will always failt in production <<<
+    assert app.debug  # this will always failt in production
     assert app.config["MAIL_SERVER"]
     assert app.config["SECRET_KEY"] == "test"
     assert app.config["SQLALCHEMY_DATABASE_URI"] == "sqlite:///../instance/app.sqlite"

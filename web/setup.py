b99e4ed7b5bf323bfc7fdc8aa963efbd506459aa
Gabriel Fioravante Jorge <GFJorge@santafilomena.local>
2020-09-02 12:17:09 -0300

    refac organization

---
+++ web/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name="flask-starter",
+    version="0.1.1",
+    install_requires=[
+        "flask",
+        "flask-login",
+        "flask-mail",
+        "flask-migrate",
+        "flask-sqlalchemy",
+        "flask-wtf",
+        "gunicorn",
+        "jwt",
+        "pytest",
+        "redis",
+        "rq",
+        "pip-tools",
+        "pre-commit"
+    ]
+)

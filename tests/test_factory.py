c305b1f325e6c8b49806cfeeea6af46c528779a5
northernSage <gfvante@gmail.com>
2020-09-08 18:02:47 -0300

    add task tests

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,4 +1,12 @@
+from app import create_app
+
+
 def test_pytest_environment(app):
     assert app.debug
     assert app.config["SECRET_KEY"] == "test"
     assert app.config["SQLALCHEMY_DATABASE_URI"] == "sqlite:///../instance/app.sqlite"
+
+
+def test_create_app(app):
+    test_app = create_app() or None
+    assert test_app is not None
--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -5,11 +5,15 @@ from app.models import Task
 from tests.conftest import AuthActions
 
 
-def test_task_enqueue(client):
+def _clean_set_up(client):
     actions = AuthActions(client)
     actions.register_and_login()
     Task.query.delete()
     db.session.commit()
+
+
+def test_task_enqueue(client):
+    _clean_set_up(client)
     rv = client.get("/tasktest/1/")
     assert rv.status_code == 302
     assert rv.headers["Location"] == r"http://localhost/"
@@ -17,12 +21,41 @@ def test_task_enqueue(client):
     assert len(tasks) == 1
 
 
+def test_task_multiple_enqueues(client):
+    _clean_set_up(client)
+    for _ in range(3):
+        client.get("/tasktest/0/")
+    sleep(1)
+    tasks = Task.query.all()
+    assert len(tasks) == 3
+
+
 def test_task_should_complete(client):
-    actions = AuthActions(client)
-    actions.register_and_login()
-    Task.query.delete()
-    db.session.commit()
-    client.get("/tasktest/0/")
+    _clean_set_up(client)
+    for _ in range(3):
+        client.get("/tasktest/0/")
     sleep(5)
-    task = Task.query.first()
-    assert task.complete
+    tasks = Task.query.all()
+    for task in tasks:
+        assert task.complete
+
+
+def test_per_user_task_separation(client):
+    _clean_set_up(client)
+    client.get("/tasktest/10/")
+    task = Task.query.filter_by(user_id=1).first()
+    assert task
+    # logout default test user
+    AuthActions(client).logout()
+    actions = AuthActions(
+        client, username="appuser2", password="appuser2", email="appuser2@gmail.com"
+    )
+    actions.register_and_login()
+    task = Task.query.filter_by(user_id=2).first()
+    assert not task
+
+
+#    client.get("/tasktest/10/")
+
+# task = Task.query.filter_by(user_id=2).first()
+# assert task
